90bc21ed0833efb0892ced9a2adc43f1a9899f5a
Giovanni Vasquez <giovanni.vasquez@alumnos.uv.cl>
2024-09-27 08:29:11 -0300

    Entrega 1 Actualizado

--- API/database.py
+++ API/database.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker
 # URL de conexión a MySQL
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:hpta@localhost/memeologia_bd"
 
+# Establece la conexión
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
